7023bfad0d5128b22e434ac550789d8a9b14e011
Rinalon <mit.krist@yandex.ru>
2026-06-22 15:48:51 +0300

    Renamed Users -> User; corrected User and PersonalData models

--- pymodels/clients/user.py
+++ pymodels/clients/user.py
@@ -18,10 +18,11 @@ from sqlalchemy.orm import (
 from sqlalchemy.dialects.postgresql import JSONB
 from pymodels.base import Base
 
-class Users(Base):
+class User(Base):
     __tablename__ = "users"
     __table_args__ = (
-        CheckConstraint("LENGTH(login) >= 6", name="ck_users_login_length"),
+        CheckConstraint("LENGTH(login) >= 6", name="users_login_check"),
+        Index("users_organisation_id_idx", "organisation_id"),
         {"schema": "clients"}
     )
 
@@ -59,8 +60,14 @@ class Users(Base):
         server_default=func.now()
     )
 
+    user_books: Mapped[list["Book"]] = relationship(back_populates="user")
+    reviews: Mapped[list["Review"]] = relationship(back_populates="user")
+    organization: Mapped["Organization"] = relationship(back_populates="user", uselist=False)
+    cheques: Mapped[list["Cheque"]] = relationship(back_populates="user")
+    personal_data: Mapped["Personaldata"] = relationship(back_populates="user")
+
 class Personaldata(Base):
-    __tablename__ = "personaldata"
+    __tablename__ = "personal_data"
     __table_args__ = (
         CheckConstraint(
             "email ~ '^([\\w]+[.-]{0,1}[\\w]+)+@([a-zA-Z0-9]+([.-])*[a-zA-Z0-9]+)+\\.+[a-zA-Z]{2,}$'",
@@ -74,41 +81,40 @@ class Personaldata(Base):
             "email IS NOT NULL OR phonenumber IS NOT NULL",
             name="email_or_phone_required",
         ),
+        Index("personaldata_payment_idx", "payment", postgresql_using='gin'),
         {"schema": "clients"}
     )
 
     user_id: Mapped[int] = mapped_column(
         Integer,
-        ForeignKey(
-            "clients.users.user_id",
-            deferrable = True,
-            initially = "DEFERRED"
-    ),
+        ForeignKey("clients.users.user_id",),
         primary_key=True,
     )
 
-    email: Mapped[str] = mapped_column(
+    email: Mapped[Optional[str]] = mapped_column(
         String(256),
         unique = True,
     )
 
-    phonenumber: Mapped[str] = mapped_column(
+    phonenumber: Mapped[Optional[str]] = mapped_column(
         String(20),
         unique=True,
     )
 
-    surname: Mapped[str] = mapped_column(
+    surname: Mapped[Optional[str]] = mapped_column(
         String(256),
     )
-    first_name: Mapped[str] = mapped_column(
+    first_name: Mapped[Optional[str]] = mapped_column(
         String(256),
     )
-    second_name: Mapped[str] = mapped_column(
+    second_name: Mapped[Optional[str]] = mapped_column(
         String(256),
     )
     payment: Mapped[Optional[dict]] = mapped_column(
         JSONB,
         nullable=True,
     )
+
+    user: Mapped["User"] = relationship(back_populates="personal_data")
 
 
