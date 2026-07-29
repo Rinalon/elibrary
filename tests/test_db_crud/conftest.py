e95f47c10a6a1b8a37e5aa9a76d41d4d3c7394c3
Rinalon <mit.krist@yandex.ru>
2026-07-29 14:03:55 +0300

    test: create conftest for crud tests

--- tests/test_db_crud/conftest.py
+++ tests/test_db_crud/conftest.py
@@ -2,8 +2,8 @@ import pytest
 from core import settings, engine
 from db.models import Base
 
-@pytest.fixture(scope='session', autouse=True)
+@pytest.fixture(scope='session', autouse=False)
 def setup_db():
     assert settings.DEBUG == True
-    Base.metadata.drop_all(engine)
-    Base.metadata.create_all(engine)
+    #Base.metadata.drop_all(engine)
+    #Base.metadata.create_all(engine)
