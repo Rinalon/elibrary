b303fb314d8dc68db9e74171ab574e71e18ca606
Rinalon <mit.krist@yandex.ru>
2026-08-04 16:40:20 +0300

    test(crud): add test for book_crud.create()

---
+++ tests/test_db_crud/test_books.py
@@ -0,0 +1,12 @@
+import pytest
+from db.crud import book_crud
+from db.schemas import BookCreate
+from tests.data.book import base_book
+
+async def test_create_book_valid(db_session):
+    data = BookCreate(**base_book)
+    new_book = await book_crud.create(db_session, data)
+
+    assert new_book is not None
+    assert new_book.title == base_book["title"]
+
