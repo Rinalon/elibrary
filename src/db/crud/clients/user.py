45d38aaebfb400b5b9441fcac28517fa67eda2f5
Rinalon <mit.krist@yandex.ru>
2026-08-04 17:13:28 +0300

    feat(crud): create UserCRUD - delete old functions (get_user_by_id, create_user, get_user_by_login) - create UserCRUD class inheriting from BaseCRUD - add method: get_by_login

--- src/db/crud/clients/user.py
+++ src/db/crud/clients/user.py
@@ -1,22 +1,12 @@
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from src.db.models import User
+from src.db.schemas import UserCreate
+from src.db.crud.base import BaseCRUD
 
-async def get_user_by_id(db: AsyncSession, user_id: int) -> User | None:
-    result = await db.execute(
-        select(User).where(User.user_id == user_id)
-    )
-    return result.scalar_one_or_none()
-
-async def get_user_by_login(db: AsyncSession, login: str) -> User | None:
-    result = await db.execute(
-        select(User).where(User.login == login)
-    )
-    return result.scalar_one_or_none()
-
-async def create_user(db: AsyncSession, user_data: dict) -> User:
-    db_user = User(**user_data)
-    db.add(db_user)
-    await db.commit()
-    await db.refresh(db_user)
-    return db_user
+class UserCRUD(BaseCRUD[User, UserCreate]):
+    async def get_by_login(self, db: AsyncSession, login: str) -> User | None:
+        result = await db.execute(
+            select(User).where(User.login == login)
+        )
+        return result.scalar_one_or_none()
