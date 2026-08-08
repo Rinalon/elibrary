5e9a0eeadf1ff8e47496ad1d2da1839f36ccedc5
Rinalon <mit.krist@yandex.ru>
2026-08-08 16:06:33 +0300

    refactor(test): change conftets - delete event_loop() fixture - move db_session fixture to tests/conftest - delete conftest from test_db_crud - rename test_db_crud -> test_crud - add sql sript for initial test db to tests/data and .gitignore

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,10 @@ import platform
 import pytest
 from pathlib import Path
 
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from src.core.config import settings
+from src.db.models import Base
+
 if platform.system() == 'Windows':
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
@@ -11,11 +15,23 @@ root_dir = Path(__file__).parent.parent
 sys.path.insert(0, str(root_dir))
 
 
-@pytest.fixture(scope="session")
-def event_loop():
-    """Создаёт новый event loop для тестовой сессии."""
-    policy = asyncio.get_event_loop_policy()
-    loop = policy.new_event_loop()
-    asyncio.set_event_loop(loop)  # Важно: установить loop как текущий
-    yield loop
-    loop.close()
+@pytest.fixture(scope="function")
+async def db_session():
+    """Создаёт тестовую сессию БД для CRUD-тестов."""
+
+    engine = create_async_engine(
+        settings.database_url_async,
+        echo=False,
+    )
+
+    session_maker = async_sessionmaker(
+        engine, class_=AsyncSession, expire_on_commit=False
+    )
+
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+    async with session_maker() as session:
+        yield session
+
+    await engine.dispose()
